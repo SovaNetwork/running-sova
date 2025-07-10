658cd3cb4040bee060fe35e293d227761636eb2d
PowVT <epowlowsky@gmail.com>
2025-07-10 16:54:33 -0400

    validator updates

--- scripts/get-sequencer-info.sh
+++ scripts/get-sequencer-info.sh
@@ -8,6 +8,11 @@ PEER_ID=$(curl -s -X POST \
   -d '{"jsonrpc":"2.0","method":"opp2p_self","params":[],"id":1}' \
   http://localhost:9545 | jq -r '.result.peerID')
 
+if [ "$PEER_ID" = "null" ] || [ -z "$PEER_ID" ]; then
+    echo "Error: Could not get peer ID from sequencer"
+    exit 1
+fi
+
 # Get public IP
 PUBLIC_IP=$(curl -s ifconfig.me)
 
@@ -15,8 +20,9 @@ echo ""
 echo "Sequencer Info:"
 echo "=================="
 echo "HTTP RPC: http://$PUBLIC_IP:8545"
-echo "P2P Bootnode: /ip4/$PUBLIC_IP/tcp/9003/p2p/$PEER_ID"
+echo "P2P Bootnode: /ip4/$PUBLIC_IP/tcp/9222/p2p/$PEER_ID"
 echo ""
 echo "Validator .env variables:"
 echo "SEQUENCER_HTTP_RPC=http://$PUBLIC_IP:8545"
-echo "SEQUENCER_P2P_BOOTNODE=/ip4/$PUBLIC_IP/tcp/9003/p2p/$PEER_ID"
+echo "SEQUENCER_P2P_BOOTNODE=/ip4/$PUBLIC_IP/tcp/9222/p2p/$PEER_ID"
+echo "SEQUENCER_BOOTNODE=http://$PUBLIC_IP:8545"  # For reth --rollup.sequencer-http
